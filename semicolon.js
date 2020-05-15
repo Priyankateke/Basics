ed8f893c235e0f696beb6e7585e37ab327fe7687
Priyankateke <priiyankateke1705@gmail.com>
2020-05-15 18:22:51 +0530

    ADDED: Semicolons

---
+++ semicolon.js
@@ -0,0 +1,7 @@
+var a, b, c;     // Declare 3 variables
+a = 5;           // Assign the value 5 to a
+b = 6;           // Assign the value 6 to b
+c = a + b;       // Assign the sum of a and b to c
+
+//When separated by semicolons, multiple statements on one line are allowed:
+a = 5; b = 6; c = a + b;
