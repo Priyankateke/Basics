0b3a08aec14a8fda8c953ba57fae8e7030885bdd
Priyankateke <priiyankateke1705@gmail.com>
2020-05-15 18:44:38 +0530

    ADDED: String Operations

---
+++ stringOperation.js
@@ -0,0 +1,15 @@
+var txt1 = "John";
+var txt2 = "Doe";
+var txt3 = txt1 + " " + txt2;
+console.log(txt3)
+
+var txt4 = "What a very ";
+txt4 += "nice day";
+console.log(txt4)
+
+var x = 5 + 5;
+var y = "5" + 5;
+var z = "Hello" + 5;
+console.log(x)
+console.log(y)
+console.log(z)
