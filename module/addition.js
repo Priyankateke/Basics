6ded6eff72f7ba41be2d0f4edf5772d88975e58f
Priyankateke <priiyankateke1705@gmail.com>
2020-05-18 20:05:02 +0530

    ADDED: User defined module

---
+++ module/addition.js
@@ -0,0 +1,9 @@
+function add() {
+    console.log("This is Addition module ")
+}
+
+var a=20;
+
+//export
+module.exports.addition=add;
+module.exports.a_val=a
---
+++ module/main.js
@@ -0,0 +1,4 @@
+
+var cal = require("./addition");
+cal.addition();
+console.log( "a :"+cal.a_val);
