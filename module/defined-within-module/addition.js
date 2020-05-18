4cfd983058dfffdc191a6b6586d168695617186f
Priyankateke <priiyankateke1705@gmail.com>
2020-05-18 20:55:10 +0530

    ADDED: Define properties in module

---
+++ module/defined-within-module/addition.js
@@ -0,0 +1,6 @@
+module.exports={
+    addition:function(){
+        console.log("This is addition module");
+    },
+    a_val:20
+}
---
+++ module/defined-within-module/main.js
@@ -0,0 +1,3 @@
+var cal = require("./addition")
+cal.addition()
+console.log("a value : "+cal.a_val)
